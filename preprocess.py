ba7e5fb34042dc01e3aad48076dc6afd001b174f
Oxel40 <Oxel40@users.noreply.github.com>
2023-06-02 08:50:26 +0200

    fix export of dataset

--- preprocess.py
+++ preprocess.py
@@ -21,17 +21,45 @@ def filter_mad(arr):
     return arr[poi, :, :]
 
 
+def export_dataset(no_taps, soft_taps, hard_taps):
+    x = np.concatenate((
+        no_taps.reshape((no_taps.shape[0], -1)),
+        soft_taps.reshape((soft_taps.shape[0], -1)),
+        hard_taps.reshape((hard_taps.shape[0], -1))
+    ))
+    y = np.concatenate((
+        np.repeat(0, no_taps.shape[0]),
+        np.repeat(1, soft_taps.shape[0]),
+        np.repeat(2, hard_taps.shape[0])
+    ))
+    y = np.eye(3)[y]
+
+    print("X shape", x.shape)
+    print("Y shape", y.shape)
+
+    assert(x.shape[0] == y.shape[0])
+
+    shuffle = np.random.shuffle(np.arange(x.shape[0]))
+    x = x[shuffle]
+    y = y[shuffle]
+
+    np.save("data/input.npy", x)
+    np.save("data/output.npy", y)
+
+
 if __name__ == "__main__":
     ht = roll_from_csv("data/hardtap.csv")
     tot_frames = ht.shape[0]
     ht = filter_mad(ht)
     intresting_frames = ht.shape[0]
-    print("TPS:", tot_frames / intresting_frames)
+    print("hard tap TPS:", tot_frames / intresting_frames)
 
     st = roll_from_csv("data/softtap.csv")
     tot_frames = st.shape[0]
     st = filter_mad(st)
     intresting_frames = st.shape[0]
-    print("TPS:", tot_frames / intresting_frames)
+    print("soft tap TPS:", tot_frames / intresting_frames)
 
     nt = roll_from_csv("data/notap.csv")
+
+    export_dataset(nt, st, ht)
